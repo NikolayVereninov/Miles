47606dd6192457b9a4c1dc2b9294b4c4618612a4
NikolayVereninov <kolia19741974@gmail.com>
2021-12-10 21:38:41 +0300

    add file

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int price = 500;
+        int ticket = 200;
+        int amount_miles_bonus = 10;
+        float amount_price = price - ticket;
+
+        System.out.println(amount_miles_bonus = 10);
+    }
+}
+
