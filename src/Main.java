755b0cb2fa57d11a559e5198b280b1d9dce015e3
NikolayVereninov <89935534+NikolayVereninov@users.noreply.github.com>
2021-12-19 14:02:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 100;
-        int addSum = 5000;
+        int balance = 350;
+        int addMoney = 2000;
         int bonus = 0;
-        if (addSum > 1000) {
-            bonus = addSum / 100;
-            int total = balance + addSum + bonus;
-            System.out.println(bonus);
+        if (addMoney > 1000) {
+            bonus = (addMoney / 100);
         }
+        int total = balance + addMoney + bonus;
+        System.out.println(bonus + balance);
     }
 }
